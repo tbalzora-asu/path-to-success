9d26f46b3f11cdcc1005a1feae4186c367215a6a
tbalzora-asu <tbalzora@asu.edu>
2025-02-14 20:32:15 -0700

    fix html and add sltyles + script for menu

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for Mobile Menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
